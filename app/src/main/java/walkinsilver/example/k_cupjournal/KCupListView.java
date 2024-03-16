3d2e45f36108ba53745dd655379c236d3a1dbbcc
Robert Walk <haseren4@gmail.com>
2024-03-16 02:08:56 -0400

    Worked On  - KCupListView  - KCupListView: Toast Layout

--- app/src/main/java/walkinsilver/example/k_cupjournal/KCupListView.java
+++ app/src/main/java/walkinsilver/example/k_cupjournal/KCupListView.java
@@ -1,11 +1,15 @@
 package walkinsilver.example.k_cupjournal;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -28,8 +32,18 @@ public class KCupListView extends LinearLayout {
         ratingBar.setRating(kcup.getRating());
 
 
-        this.setOnClickListener(l ->{
+        this.setOnLongClickListener(l ->{
+            LayoutInflater toast_inflater = (LayoutInflater)  context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.kcup_list_view_toast_layout,
+                    (ViewGroup) findViewById(R.id.layout));
 
+            Toast toast = new Toast(context);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+
+            toast.setView(layout);
+            toast.show();
+            return false;
         });
     }
 
